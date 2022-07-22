54615f9432d991e0d3aa66a8b877ef346322a1c1
cybercortex <tamir.zehavi01@gmail.com>
2022-07-22 13:04:12 +0300

    Inserted fruit graphics + random fruit on each fire

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +7,24 @@ using UnityEngine.Events;
 public class Fruit : MonoBehaviour
 {
     public UnityEvent OnHit;
+    [SerializeField] List<Sprite> sprites = new List<Sprite>();
+
+    void Start()
+    {
+        SpriteRenderer SpriteRenderer = GetComponent<SpriteRenderer>();
+        SpriteRenderer.sprite = GetRandomSprite();
+    }
+
+    private Sprite GetRandomSprite()
+    {
+        var random = new System.Random();
+        int index = random.Next(sprites.Count);
+        return sprites[index];
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         OnHit?.Invoke();
     }
+
 }
