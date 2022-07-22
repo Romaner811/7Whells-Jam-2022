111d23487340e9013d885ff24630519ebca10e77
Roman K <romank@la-mark.com>
2022-07-22 14:46:30 +0300

    add initial basket logic

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -7,25 +7,43 @@ public class Basket : MonoBehaviour
 {
     public const string FRUIT_TAG = "Fruit";
 
-    [SerializeReference] Sensor _entrance;
     [SerializeReference] Sensor _body;
+
+    [SerializeField] int _visualCapacity;
 
     public UnityEvent OnScore;
 
-    List<Fruit>
+    Queue<Fruit> _inside;
+
+    private void Awake()
+    {
+        _inside = new Queue<Fruit>(_visualCapacity * 2);
+    }
 
     private void Start()
     {
-        
+        _body.OnEnter.AddListener(OnEnterBody);
     }
 
-    private void OnEnter()
+    private void OnEnterBody(Collider2D collider)
     {
+        if (collider.TryGetComponent<Fruit>(out var item) == false) return;
 
+        _inside.Enqueue(item);
+        item.transform.SetParent(transform);
+
+        OnScore?.Invoke();
+
+        DestroyOverflow();
     }
 
-    private void OnEnterBody()
+    public void DestroyOverflow()
     {
-
+        while (_inside.Count > _visualCapacity)
+        {
+            Fruit item = _inside.Dequeue();
+            item.gameObject.SetActive(false);
+            Destroy(item);
+        }
     }
 }
